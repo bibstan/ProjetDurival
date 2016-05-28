cbc82bae891169cd7e9226d4fc313b5e6f7f2a36
sardinecan <morvan.josselin@gmail.com>
2016-05-28 14:26:11 +0200

    fusion Eric

---
+++ edition/js/cartographie/belprey.js
@@ -0,0 +1,38 @@
+// Using leaflet.js to pan and zoom a big image.
+// See also: http://kempe.net/blog/2014/06/14/leaflet-pan-zoom-image.html
+
+// create the slippy map
+var map = L.map('mapid', {
+  minZoom: -0.5,
+  maxZoom: 3,
+  center: [6500, 5000],
+  zoom: -0.5,
+  crs: L.CRS.Simple
+});
+
+// dimensions of the image
+var w = 8000,
+    h = 5490,
+    url = '../images/plans/belpreyBD.jpg';
+
+// calculate the edges of the image, in coordinate space
+var southWest = map.unproject([0, h], map.getMaxZoom()-1);
+var northEast = map.unproject([w, 0], map.getMaxZoom()-1);
+var bounds = new L.LatLngBounds(southWest, northEast);
+
+// add the image overlay, 
+// so that it covers the entire map
+L.imageOverlay(url, bounds).addTo(map);
+
+// tell leaflet that the map is exactly as big as the image
+map.setMaxBounds(bounds);
+
+      function onMapClick(e) {
+         popup
+            .setLatLng(e.latlng)
+            .setContent("You clicked the map at " + e.latlng.toString())
+            .openOn(mymap);
+      }
+
+
+      mymap.on('click', onMapClick);
